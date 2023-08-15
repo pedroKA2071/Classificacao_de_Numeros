95ee9e59dc7f591a4da248ee8e2cb0f91cdcb0ba
Pedro Henrique Gaspar <mortadela22112005@gmail.com>
2023-08-15 10:34:07 -0300

    ordem em C#

---
+++ Classificação de Números/Classificação de Números/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classificação_de_Números
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero1, numero2, numero3;
+
+
+            Console.WriteLine("Digite o primeiro número:");
+             numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+             numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro número:");
+             numero3 = int.Parse(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                int temp = numero1;
+                numero1 = numero2;
+                numero2 = temp;
+            }
+
+            if (numero2 > numero3)
+            {
+                int temp = numero2;
+                numero2 = numero3;
+                numero3 = temp;
+            }
+
+            //if (numero1 > numero2)
+            // {
+            // int temp = numero1;
+            // numero1 = numero2;
+            // numero2 = temp;
+            // }
+
+            Console.WriteLine("Números em ordem crescente: {0}, {1}, {2}", numero1, numero2, numero3);
+
+
+            Console.ReadKey();
+        }
+    }
+}
